2306fc88c6acb585847ac0e2bcb792bc07277267
Elliot Perry <edperry1@hotmail.co.uk>
2024-02-24 19:40:51 +0000

    "added .py file"

---
+++ seqClass.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import sys, re
+from argparse import ArgumentParser
+
+parser = ArgumentParser(description = 'Classify a sequence as DNA or RNA')
+parser.add_argument("-s", "--seq", type = str, required = True, help = "Input sequence")
+parser.add_argument("-m", "--motif", type = str, required = False, help = "Motif")
+
+
+if len(sys.argv) == 1:
+    parser.print_help()
+    sys.exit(1)
+
+args = parser.parse_args()
+args.seq = args.seq.upper()
+if re.search('^[ACGTU]+$', args.seq):
+    if re.search('T', args.seq):
+        print ('is DNA')
+    elif re.search('U', args.seq):
+        print ('is RNA')
+    else:
+        print ('The sequence can be DNA or RNA')
+else:
+    print ('The sequence is not DNA nor RNA')
+
+if args.motif:
+    args.motif = args.motif.upper()
+    print(f'Motif search enabled: looking for motif "{args.motif}" in sequence "{args.seq}"... ', end = '')
+    if re.search(args.motif, args.seq):
+        print("Found babay d")
+    else:
+        print("NOT FOUND")
